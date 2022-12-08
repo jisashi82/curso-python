449cde320bd595a650889eaceb9c33a4572ce189
jisashi82 <jisashi82@gmail.com>
2022-12-07 20:34:32 -0600

    video 17: Funciones utiles

---
+++ Basico/13-funcionesUtiles.py
@@ -0,0 +1,16 @@
+#redondeo
+print(round(3.14151628))
+
+#valor absoluto
+print(abs(-56))
+
+#Min y Max de una lista
+lista =[1,9,3,5,2,6,8,13,17,26,21]
+print(min(lista))
+print(max(lista))
+
+#Ordenar lista
+print(sorted(lista))
+
+#suma total de la lista
+print(sum(lista))
