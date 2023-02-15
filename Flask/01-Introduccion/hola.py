6ed93073e8b5e50b853357b0a12dd934b5ea0494
jisashi82 <jisashi82@gmail.com>
2023-02-15 08:39:42 -0600

    Introduccion a Flask

--- Flask/01-Introduccion/hola.py
+++ Flask/01-Introduccion/hola.py
@@ -14,5 +14,18 @@ def show_user(name):
     return f'Hola {name}'
 
 
+class Blog():
+    def __init__(self,titulo,desc) -> None:
+        self.titulo=titulo
+        self.desc=desc
+
+
+@app.route('/blogs')
+def blogs():
+    b1=Blog('Que es Python', 'Descripcion 1')
+    b2=Blog('Que es Flask', 'Descripcion 2')    
+    lista=[b1,b2]
+    return render_template('blogs.html',blogs=lista)
+
 if __name__=='__main__': 
     app.run(debug=True)
