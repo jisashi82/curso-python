dbed4489c4d01a7b452c9ad9e2cb2610ff9c345b
jisashi82 <jisashi82@gmail.com>
2023-02-20 20:33:38 -0600

    Video 174-177: Vistas y Herencia de Plantillas

--- Flask/web-personal/webpersonal/views.py
+++ Flask/web-personal/webpersonal/views.py
@@ -1,5 +1,14 @@
+from flask import render_template
 from webpersonal import app
 
 @app.route('/')
 def home():
-    return 'pagina de inicio'
+    return render_template('home.html',msj='pagina de inicio')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html',msj='Portfolio')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', msj='Formulario de contacto')
