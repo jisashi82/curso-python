963317a850b33f00aab22dc82f46ef690e049413
jisashi82 <jisashi82@gmail.com>
2023-02-22 19:43:44 -0600

    Video 184-185:Config de la BD y Creacion de Modelo

--- Flask/web-personal/webpersonal/project/models.py
+++ Flask/web-personal/webpersonal/project/models.py
@@ -1,12 +1,17 @@
-class Project:
-    def __init__(self, titulo, desc) -> None:
-        self.titulo=titulo
-        self.desc=desc
+from webpersonal import db
+
+class Project(db.Model):
+    
+    __tablename__='projects'
+    id = db.Column(db.Integer, primary_key=True )
+    title= db.Column(db.String(100))
+    description=db.Column(db.Text)
+    
+    def __init__(self, title, description) -> None:
+        self.title=title
+        self.description=description
+        
+    def __repr__(self) -> str:
+        return f'Project: {self.title}'
         
 
-p1= Project('Proyecto 01', 'Descripcion del Proyecto 01')
-p2= Project('Proyecto 02', 'Descripcion del Proyecto 03')
-p3= Project('Proyecto 03', 'Descripcion del Proyecto 03')
-
-
-PROYECTS =[p1,p2,p3]
--- Flask/web-personal/webpersonal/project/views.py
+++ Flask/web-personal/webpersonal/project/views.py
@@ -1,9 +1,11 @@
 from flask import render_template, Blueprint
-from webpersonal.project.models import PROYECTS
+#from webpersonal.project.models import PROYECTS
+from webpersonal.project.models import Project
 
 project =Blueprint('project', __name__)
 
 @project.route('/projects')
 @project.route('/projects/index')
 def index():
-    return render_template('project/index.html', projects=PROYECTS)
+    #return render_template('project/index.html', projects=PROYECTS)
+    return 'Cargar Modelo'
