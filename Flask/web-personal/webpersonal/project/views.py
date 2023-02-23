ae881d041241861a83b85418733a7cdb0ade4d3f
jisashi82 <jisashi82@gmail.com>
2023-02-23 08:33:30 -0600

    Video 186:  Editando la vista y  query a la BD

--- Flask/web-personal/webpersonal/project/views.py
+++ Flask/web-personal/webpersonal/project/views.py
@@ -1,11 +1,14 @@
 from flask import render_template, Blueprint
 #from webpersonal.project.models import PROYECTS
 from webpersonal.project.models import Project
+from webpersonal import db
 
 project =Blueprint('project', __name__)
 
 @project.route('/projects')
 @project.route('/projects/index')
 def index():
+    projects = Project.query.all()
+    db.session.commit()
     #return render_template('project/index.html', projects=PROYECTS)
-    return 'Cargar Modelo'
+    return render_template('project/index.html', projects=projects)
