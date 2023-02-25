03913b9830600b8c335558d9bd3524c5ead10c15
jisashi82 <jisashi82@gmail.com>
2023-02-25 14:43:51 -0600

    Video 204: Retrieve data & Listing into HTML view

--- Django/01-Introduccion/MyWeb/blog/views.py
+++ Django/01-Introduccion/MyWeb/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
+from .models import Post
 
 # Create your views here.
 def index(request):
     #return HttpResponse('Hola Mundo con Django')
-    return render(request, 'index.html')
+    post =Post.objects.all()
+    return render(request, 'index.html', {'posts':post})
