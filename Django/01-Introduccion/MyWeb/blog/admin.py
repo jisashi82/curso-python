386fc2ae55a87f4a335baa12aa2d1ef902f7ccb0
jisashi82 <jisashi82@gmail.com>
2023-02-25 13:04:03 -0600

    Video 202: Panel de Administrador

--- Django/01-Introduccion/MyWeb/blog/admin.py
+++ Django/01-Introduccion/MyWeb/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+admin.site.register(Post)
+
+""" Ejecutar el comando-> py manage.py createsuperuser 
+    para crear el superusuario del Panel de Administrador
+"""
