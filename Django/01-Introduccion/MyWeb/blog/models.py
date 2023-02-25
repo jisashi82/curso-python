ad1f0ce46288d24f33b8ad767b83667f9acf53c4
jisashi82 <jisashi82@gmail.com>
2023-02-25 11:29:15 -0600

    Video 201: Migrar Aplicacion Blog

--- Django/01-Introduccion/MyWeb/blog/models.py
+++ Django/01-Introduccion/MyWeb/blog/models.py
@@ -7,3 +7,11 @@ class Post(models.Model):
     
     def __str__(self) -> str:
         return self.title
+
+
+"""
+1.-Preparar la migracion con el comando-> py manage.py makemigrations blog 
+   esto genera el archivo 0001_initial.py en la carpeta  migrations
+2.-Visualizar  el SQL con el comando-> py manage.py sqlmigrate blog 0001
+3.-Realizar la migracion con el comando-> py manage.py migrate blog
+"""
