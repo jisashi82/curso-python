c1410d524b1a76491d9a12086108d2c9db8e1ef4
jisashi82 <jisashi82@gmail.com>
2023-03-03 22:38:33 -0600

    include routers in main

--- FastApi/Backend/routers/products.py
+++ FastApi/Backend/routers/products.py
@@ -1,7 +1,15 @@
 from fastapi import APIRouter
 
-router= APIRouter()
+router= APIRouter(prefix='/products', responses={404:{'message':'No Encontrado'}})
 
-@router.get('/products')
+products_list = ['Producto 1','Producto 2','Producto 3','Producto 4', 'Producto 5']
+
+@router.get('/')
 async def home():
-    return ['Producto 1','Producto 2','Producto 3','Producto 4', 'Producto 5']
+    return products_list
+
+
+@router.get('/{id}')
+async def home(id:int):
+    return products_list[id]
+
--- FastApi/Backend/routers/users.py
+++ FastApi/Backend/routers/users.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import APIRouter,HTTPException
 from pydantic import BaseModel
 
-app= FastAPI()
+router= APIRouter()
 
-#Inicia el server: uvicorn users:app --reload
+#Inicia el server: uvicorn users:router --reload
 
 #Entidad User
 class User(BaseModel):
@@ -17,24 +17,24 @@ users_list=[User(id=1,name='Abel',surname='Ramos',telefono='96214413326',age=41)
             User(id=2,name='Jisashi',surname='Nakamura',telefono='9624445666',age=41),
             User(id=3,name='Said',surname='Ramos',telefono='9616789876',age=35)]
 
-@app.get('/usersjson')
+@router.get('/usersjson')
 async def usersjson():
     return [
         {'name': 'Jisashi', 'surname':'nakamura','telefono':'9621334567', 'age':41},
         {'name': 'Abel', 'surname':'Ramos','telefono':'9611634968', 'age':20},
         ]
     
-@app.get('/users')
+@router.get('/users')
 async def users():
     return users_list
 
-@app.get('/user/{id}')
+@router.get('/user/{id}')
 async def user(id:int):
     #llamar el path: http://localhost:8000/user/2
     return search_user(id)
         
 
-@app.get('/user/')
+@router.get('/user/')
 async def user(id:int):
     #llamar el path: http://localhost:8000/usersquery?id=2
     return search_user(id)
@@ -48,7 +48,7 @@ def search_user(id:int):
         return {'error':'No se ha encontrado el usuario'}
     
 
-@app.post('/user/',status_code=201)
+@router.post('/user/',status_code=201)
 async def user(user:User):
     if type(search_user(user.id)) == User:
         raise HTTPException(status_code=204, detail='El usuario ya existe')
@@ -57,7 +57,7 @@ async def user(user:User):
         users_list.append(user)
         return {'message': 'Se ha insertado el usuario'}
     
-@app.put('/user/')
+@router.put('/user/')
 async def user(user:User):
     found=False
     
@@ -73,7 +73,7 @@ async def user(user:User):
         return {'message':'Se ha actualizado el usuario exitosamente'}
     
 
-@app.delete('/user/{id}')
+@router.delete('/user/{id}')
 async def user(id:int):
     found=False
     
